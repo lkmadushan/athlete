b73c131bbaca1e44ae3e689f672bbd5e3f94621a
Kalpa Madushan Perera <kalpa.perera@elegantmedia.lk>
2015-03-30 15:31:51 +0530

    Updated API docs

--- app/Athlete/Requests/RegisterUserRequest.php
+++ app/Athlete/Requests/RegisterUserRequest.php
@@ -7,7 +7,7 @@ class RegisterUserRequest extends FormValidator {
 	protected $rules = [
 		'device_id' => 'unique:devices,id|required',
 		'device_type' => 'required',
-		'push_token' => 'unique:devices|required',
+		'push_token' => 'unique:devices',
 		'email' => 'unique:users|required|email',
 		'password' => 'required'
 	];
