437fd730d511b058c6dcbc1a0ab009c85814de4d
Kalpa Madushan Perera <kalpa.lkm@gmail.com>
2015-04-28 22:02:01 +0530

    Created skills controller

---
+++ app/controllers/SkillsController.php
@@ -0,0 +1,63 @@
+<?php
+
+class SkillsController extends \ApiController {
+
+	/**
+	 * Display a listing of the resource.
+	 * GET /skills
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 * POST /skills
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 * GET /skills/{id}
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 * PUT /skills/{id}
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 * DELETE /skills/{id}
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
