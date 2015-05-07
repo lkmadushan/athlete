f45a94f9d013d4f3aca899b8adae7e2569067b4c
Kalpa Madushan Perera <kalpa.perera@elegantmedia.lk>
2015-05-07 09:33:21 +0530

    Updated password reset email

--- app/views/emails/auth/reminder.blade.php
+++ app/views/emails/auth/reminder.blade.php
@@ -4,15 +4,12 @@
 		<meta charset="utf-8">
 	</head>
 	<body>
-		<h2>Password Reset</h2>
-
 		<div>
             <p>Dear user,</p>
 
-            <p>You recently requested to reset your password.</p>
-
-			<p>To reset your password, complete this form: {{ URL::to('password/reset', array($token)) }}.<br/>
-			This link will expire in {{ Config::get('auth.reminder.expire', 60) }} minutes.</p>
+            <p>You recently requested to reset your password.<br/>
+                To reset your password, complete this form: {{ URL::to('password/reset', array($token)) }}.<br/>
+                This link will expire in {{ Config::get('auth.reminder.expire', 60) }} minutes.</p>
 		</div>
 	</body>
 </html>
